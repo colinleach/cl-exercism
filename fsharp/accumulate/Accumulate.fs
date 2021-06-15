718400b6e5ad25ef8b66f2044c9a50bcc9267d8e
Colin Leach <colin.leach@comcast.net>
2021-06-15 14:31:57 -0700

    more F# solutions

---
+++ fsharp/accumulate/Accumulate.fs
@@ -0,0 +1,18 @@
+﻿module Accumulate
+
+let rec private accumHelper accumulator func xs =
+    match xs with
+    | []    -> accumulator
+    | y::ys -> accumHelper ((func y)::accumulator) func ys 
+    // NB output list is reversed for efficiency
+
+let rec private reverse accumulator xs = 
+    match xs with
+    | []    -> accumulator
+    | y::ys -> reverse (y::accumulator) ys
+ 
+let accumulate (func: 'a -> 'b) (input: 'a list): 'b list = 
+    let revOutput = accumHelper [] func input
+    // put the list back in the correct order
+    reverse [] revOutput
+    
