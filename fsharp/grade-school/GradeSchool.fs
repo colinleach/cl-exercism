43d6a5276380565dd6eb1325288b12d657840429
Colin Leach <colin.leach@comcast.net>
2021-06-16 10:08:02 -0700

    another F# solution

--- fsharp/grade-school/GradeSchool.fs
+++ fsharp/grade-school/GradeSchool.fs
@@ -3,13 +3,23 @@
 type School = Map<int, string list>
 
 let empty: School = 
-    failwith "You need to implement this function."
+    Map.empty
 
 let add (student: string) (grade: int) (school: School): School = 
-    failwith "You need to implement this function."
+    match (Map.containsKey grade school) with
+    | true -> 
+        let students = List.sort (student :: Map.find grade school)
+        school.Add (grade, students)
+    | false -> 
+        school.Add (grade, [student])
 
 let roster (school: School): string list = 
-    failwith "You need to implement this function."
+    Map.fold 
+        (fun acc key value -> (acc @ value))
+        []
+        school
 
 let grade (number: int) (school: School): string list = 
-    failwith "You need to implement this function."
+    match (Map.containsKey number school) with
+    | true  -> Map.find number school
+    | false -> []
