c16ec3ec86c9be24b3ccf18e33353e368402feb1
Colin Leach <colin.leach@comcast.net>
2021-08-03 16:55:29 -0700

    more rust

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,3 +1,24 @@
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    let mut primes = vec![2, 3, 5, 7, 11, 13, 17];
+    while primes.len() <= n as usize {
+        primes.push(next_prime(&primes));
+    }
+    primes[n as usize]
+}
+
+fn next_prime(prev: &Vec<u32>) -> u32 {
+    let mut next_try = prev.last().unwrap() + 2;
+    while !is_prime(&next_try, &prev) {
+        next_try += 2;
+    }
+    next_try
+}
+
+fn is_prime(num: &u32, prev: &Vec<u32>) -> bool {
+    for i in prev {
+        if num % i == 0 {
+            return false;
+        }
+    }
+    true
 }
