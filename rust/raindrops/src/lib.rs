36cce11ac6f1178097efcb61385ce8dd0958430d
Colin Leach <colin.leach@comcast.net>
2021-08-03 15:51:25 -0700

    Rust ugly first attempt

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,24 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let pling = if n % 3 == 0 {
+        String::from("Pling")
+    } else {
+        String::from("")
+    };
+    let plang = if n % 5 == 0 {
+        String::from("Plang")
+    } else {
+        String::from("")
+    };
+    let plong = if n % 7 == 0 {
+        String::from("Plong")
+    } else {
+        String::from("")
+    };
+
+    let drops = pling + &plang + &plong;
+    if drops.len() == 0 {
+        n.to_string()
+    } else {
+        drops
+    }
 }
