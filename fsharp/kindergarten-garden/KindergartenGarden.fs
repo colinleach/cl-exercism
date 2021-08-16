71dadf8f679ac72cd63e42f969d4caa751156fa3
Colin Leach <colin.leach@comcast.net>
2021-08-16 10:19:37 -0700

    F# solution

---
+++ fsharp/kindergarten-garden/KindergartenGarden.fs
@@ -0,0 +1,36 @@
+﻿module KindergartenGarden
+
+type Plant = 
+| Clover
+| Grass
+| Radishes
+| Violets
+
+let students = 
+    [   "Alice", 0;
+        "Bob", 1;
+        "Charlie", 2;
+        "David", 3;
+        "Eve", 4;
+        "Fred", 5;
+        "Ginny", 6;
+        "Harriet", 7;
+        "Ileana", 8;
+        "Joseph", 9;
+        "Kincaid", 10;
+        "Larry", 11]
+    |> dict
+
+let findPlant abbrev = 
+    match abbrev with
+    | 'C' -> Plant.Clover
+    | 'G' -> Plant.Grass
+    | 'R' -> Plant.Radishes
+    | 'V' -> Plant.Violets
+
+let plants (diagram: string) student = 
+    let rows = diagram.Split '\n'
+    let studentVal = 2 * students.[student]
+    let planted = rows.[0].[studentVal..(studentVal+1)] + 
+                  rows.[1].[studentVal..(studentVal+1)]
+    planted |> Seq.toList |> List.map findPlant
