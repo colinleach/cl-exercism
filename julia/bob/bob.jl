d654a4156ff9f1a497971e5713366b8ed0cbd89a
colinleach <colin.leach@comcast.net>
2021-09-05 14:42:26 -0700

    more Julia

--- julia/bob/bob.jl
+++ julia/bob/bob.jl
@@ -1,3 +1,23 @@
-function bob(stimulus)
+isEmpty(input) = isempty(input) || !occursin(r"\S", input)
+isUpper(input) = occursin(r"[A-Z]", input) && input == uppercase(input)
+isQuestion(input) = occursin(r"\?\s*$", input)
 
+function bob(stimulus)
+    if isEmpty(stimulus)
+        return "Fine. Be that way!"
+    end
+    if isUpper(stimulus)
+        if isQuestion(stimulus)
+            resp = "Calm down, I know what I'm doing!"
+        else
+            resp = "Whoa, chill out!"
+        end
+    else
+        if isQuestion(stimulus)
+            resp = "Sure."
+        else
+            resp = "Whatever."
+        end
+    end
+    resp
 end
