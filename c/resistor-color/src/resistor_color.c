14d459c2eaad87f2aae59d33181889d5610f57e7
Colin Leach <colin.leach@comcast.net>
2021-11-06 10:56:25 -0700

    C example

--- c/resistor-color/src/resistor_color.c
+++ c/resistor-color/src/resistor_color.c
@@ -1,1 +1,14 @@
 #include "resistor_color.h"
+#include <stdlib.h>
+
+int color_code(resistor_band_t color) {    
+    return (int)color;
+}
+
+resistor_band_t * colors() {
+    static resistor_band_t cols[] = {
+        BLACK, BROWN, RED, ORANGE, YELLOW,
+        GREEN, BLUE, VIOLET, GREY, WHITE
+   };
+   return cols;
+}
