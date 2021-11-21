810f2d7a7f937318d817a077d71fb2f75f0910e0
colinleach <colin.leach@comcast.net>
2021-11-21 16:07:23 -0700

    various F#

---
+++ fsharp/reverse-string/ReverseString.fs
@@ -0,0 +1,9 @@
+﻿module ReverseString
+
+let rec reverseHelper(forwardString: string, reverseString: string): string =
+    match forwardString with
+    | "" -> reverseString
+    | _ -> reverseHelper(forwardString.[0..forwardString.Length-2],
+                         reverseString + forwardString.Substring(forwardString.Length-1))
+let reverse (input: string): string =
+    reverseHelper(input, "")
