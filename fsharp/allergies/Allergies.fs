0fc1143bf388d8a44befe0f54a5f90d06dc9f89f
Colin Leach <colin.leach@omcast.net>
2021-11-26 10:52:34 -0700

    2 more F#

---
+++ fsharp/allergies/Allergies.fs
@@ -0,0 +1,35 @@
+﻿module Allergies
+
+open System
+
+type Allergen =
+    | Eggs
+    | Peanuts
+    | Shellfish
+    | Strawberries
+    | Tomatoes
+    | Chocolate
+    | Pollen
+    | Cats
+    
+let allergens = [Allergen.Eggs; Allergen.Peanuts; Allergen.Shellfish; Allergen.Strawberries;
+                Allergen.Tomatoes; Allergen.Chocolate; Allergen.Pollen; Allergen.Cats]
+
+let powerOf2 n = 1 <<< n
+
+let getIndex allergen = allergens |> List.findIndex (fun x -> x = allergen)
+
+let allergicTo codedAllergies allergen =
+    let inx = getIndex allergen
+    let code = powerOf2 inx
+    (codedAllergies &&& code) > 0
+
+let listBuilder currList codedAllergies allergen =
+    if allergicTo codedAllergies allergen then allergen :: currList
+    else currList
+    
+let list codedAllergies =
+    ([], [0..7])
+    ||> Seq.fold (fun currList inx -> (listBuilder currList codedAllergies allergens.[inx]))
+    |> List.rev
+            
---
+++ fsharp/pangram/Pangram.fs
@@ -0,0 +1,6 @@
+﻿module Pangram
+
+let isPangram (input: string): bool =
+    let listLower = input.ToLower() |> Seq.toList
+    ['a'..'z'] |> List.forall (fun c -> List.contains c listLower)
+    
