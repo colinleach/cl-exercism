7787cfbf201bf300c302087b647e8e267f0e1caf
Colin Leach <colin.leach@omcast.net>
2023-01-25 12:25:09 -0700

    elm

--- elm/marios-marvellous-lasagna/src/MariosMarvellousLasagna.elm
+++ elm/marios-marvellous-lasagna/src/MariosMarvellousLasagna.elm
@@ -1,10 +1,12 @@
 module MariosMarvellousLasagna exposing (remainingTimeInMinutes)
 
-    remainingTimeInMinutes : Int -> Int
-    remainingTimeInMinutes layers elapsedMinutes =
-        let
-            expectedMinutesInOven = 40
 
-            preparationTimeInMinutes layers = layers * 2
-        in
-        layers + expectedMinutesInOven - elapsedMinutes
+remainingTimeInMinutes : Int -> Int -> Int
+remainingTimeInMinutes layers elapsedMinutes =
+    let
+        expectedMinutesInOven = 40
+
+        preparationTimeInMinutes = layers * 2
+
+    in
+        preparationTimeInMinutes + expectedMinutesInOven - elapsedMinutes
