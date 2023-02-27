68e2f4ba0f12a2828a8144671af1208aa7a2ae6a
colinleach <colin.leach@comcast.net>
2023-02-27 13:03:16 -0700

    c++

--- cpp/reverse-string/reverse_string.h
+++ cpp/reverse-string/reverse_string.h
@@ -1,8 +1,13 @@
 #if !defined(REVERSE_STRING_H)
 #define REVERSE_STRING_H
 
-namespace reverse_string {
+#include <string>
+#include <algorithm>
 
+using namespace std;
+
+namespace reverse_string {
+    string reverse_string(string);
 }  // namespace reverse_string
 
 #endif // REVERSE_STRING_H
