7d5af2dc9db6fd3bfb2df7fa3076323d6e6c8bd2
Colin Leach <colin.leach@omcast.net>
2023-05-29 15:59:41 -0700

    ruby

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,8 +1,6 @@
-=begin
-Write your code for the 'Resistor Color Duo' exercise in this file. Make the tests in
-`resistor_color_duo_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/resistor-color-duo` directory.
-=end
-
+class ResistorColorDuo
+  def self.value(bands)
+    colors = %w[black brown red orange yellow green blue violet grey white]
+    colors.find_index(bands[0]) * 10 + colors.find_index(bands[1])
+  end
+end
