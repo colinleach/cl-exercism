b9d528f3fdc3ea1861f91d81cf1a318e74dd82ab
colinleach <colin.leach@comcast.net>
2023-08-29 16:04:12 -0700

    bash

---
+++ bash/leap/leap.sh
@@ -0,0 +1,12 @@
+re='^[0-9]+$'
+if [[ $# -ne 1 || ! ($1 =~ $re) ]]; then
+    echo "Usage: leap.sh <year>"
+    exit 1
+fi
+
+if [[ $(($1 % 400)) -eq 0 || ($(($1 % 4)) -eq 0 && !($(($1 % 100)) -eq 0)) ]]
+then
+    echo "true"
+else
+    echo "false"
+fi
