d27f0267fca776e591c1ee564751a464c4a5fe7a
Colin Leach <colin.leach@omcast.net>
2023-09-27 10:33:28 -0700

    SimpleCipher complete

--- csharp/simple-cipher/SimpleCipher.cs
+++ csharp/simple-cipher/SimpleCipher.cs
@@ -3,7 +3,6 @@ using System.ComponentModel.DataAnnotations;
 
 public class SimpleCipher
 {
-    private char[] keys;
      public SimpleCipher()
      {
          Random rand = new Random();
@@ -16,39 +15,42 @@ public class SimpleCipher
              key += letter;
          }
          Key = key;
-         keys = Key.ToCharArray();
      }
 
      public SimpleCipher(string key)
      {
          Key = key;
-         keys = Key.ToCharArray();
      }
 
     public string Key { get; private set; }
 
     string Change(string input, bool encode)
     {
+        // recycle Key to make it long enough
+        while (Key.Length < input.Length) Key += Key;
+        char[] keys = Key.ToCharArray();
+        
         char[] letters = input.ToCharArray();
         string output = "";
         for (int i = 0; i < input.Length; i++)
         {
+            int ascii;
             if (encode)
-                output += Convert.ToChar((int)letters[i] + (int)keys[i] - (int)'a');
-            else
-                output += Convert.ToChar((int)letters[i] - ((int)keys[i] - (int)'a')); 
+            {
+                ascii = (int)letters[i] + (int)keys[i] - (int)'a';
+                if (ascii > (int)'z') ascii -= 26;
+            }
+            else // decode
+            {
+                ascii = (int)letters[i] - ((int)keys[i] - (int)'a');
+                if (ascii < (int)'a') ascii += 26;
+            } 
+            output += Convert.ToChar(ascii);
         }
-
         return output;
     }
     
-    public string Encode(string plaintext)
-    {
-        return Change(plaintext, true);
-    }
+    public string Encode(string plaintext) => Change(plaintext, true);
 
-    public string Decode(string ciphertext)
-    {
-        return Change(ciphertext, false);
-    }
+    public string Decode(string ciphertext) => Change(ciphertext, false);
 }
