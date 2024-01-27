968231ba662c83078bc8814a73e6c713be0449b8
Colin Leach <colin.leach@omcast.net>
2024-01-26 17:46:42 -0700

    python

--- python/list-ops/list_ops.py
+++ python/list-ops/list_ops.py
@@ -1,30 +1,36 @@
 def append(list1, list2):
-    pass
+    return list1 + list2
 
 
 def concat(lists):
-    pass
+    result = []
+    for item in lists:
+        result += item
+    return result
 
 
-def filter(function, list):
-    pass
+def filter(function, input_list):
+    return [item for item in input_list if function(item)]
 
 
-def length(list):
-    pass
+def length(input_list):
+    return sum(1 for item in input_list)
 
 
-def map(function, list):
-    pass
+def map(function, input_list):
+    return [function(item) for item in input_list ]
 
 
-def foldl(function, list, initial):
-    pass
+def foldl(function, input_list, initial):
+    result = initial
+    for item in input_list:
+        result = function(result, item)
+    return result
 
 
-def foldr(function, list, initial):
-    pass
+def foldr(function, input_list, initial):
+    return foldl(function, input_list[::-1], initial)
 
 
-def reverse(list):
-    pass
+def reverse(input_list):
+    return input_list[::-1]
