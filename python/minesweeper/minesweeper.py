a0ce7aae749516d095a289c539322b3f5de2ab31
colinleach <colin.leach@comcast.net>
2024-04-13 11:05:57 -0700

    Python

--- python/minesweeper/minesweeper.py
+++ python/minesweeper/minesweeper.py
@@ -9,16 +9,18 @@ def annotate(minefield):
         raise ValueError('The board is invalid with current input.')
 
     def count_adjacent(row, col):
-        adj_squares = [
-            (row - 1, col - 1),
-            (row - 1, col),
-            (row - 1, col + 1),
-            (row, col - 1),
-            (row, col + 1),
-            (row + 1, col - 1),
-            (row + 1, col),
-            (row + 1, col + 1),
-        ]
+        # adj_squares = [
+        #     (row - 1, col - 1),
+        #     (row - 1, col),
+        #     (row - 1, col + 1),
+        #     (row, col - 1),
+        #     (row, col + 1),
+        #     (row + 1, col - 1),
+        #     (row + 1, col),
+        #     (row + 1, col + 1),
+        # ]
+
+        adj_squares = ((row + row_diff, col + col_diff) for row_diff in (-1, 0, 1) for col_diff in (-1, 0, 1))
         neighbors = [minefield[r][c] for r, c in adj_squares
                      if 0 <= r < rows and 0 <= c < cols]
         return len([adj for adj in neighbors if adj == '*'])
