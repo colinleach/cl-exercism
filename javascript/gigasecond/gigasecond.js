a35bbd9083c7ff6c05705f41e52bd4aabe22bd5c
Colin Leach <colin.ex@pm.me>
2025-01-23 07:35:08 -0700

    R

--- javascript/gigasecond/gigasecond.js
+++
@@ -1,1 +0,0 @@
-export const gigasecond = (date) => new Date(date.getTime() + 1e12);
---
+++ r/perfect-numbers/perfect-numbers.R
@@ -0,0 +1,9 @@
+number_type <- function(n){
+  stopifnot(n > 0)
+  numrange <- 1:(n %/% 2)
+  aliquot <- sum(numrange[n %% numrange == 0])
+  
+  if (n == 1 || aliquot < n) return("deficient")
+  if (aliquot == n) return("perfect")
+  if (aliquot > n) return("abundant")
+}
